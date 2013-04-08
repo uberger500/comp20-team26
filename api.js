f47dbee10a07db00c53ef98c07778d72851d2f96
foslock <foslock@gmail.com>
2013-04-07 21:15:19 -0400

    Finished the project. Win

--- api.js
+++ api.js
@@ -3,10 +3,14 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Wingman!');
+	response.send('You\'re totally on a plane. It\'s in the sky. It\'s going fast.');
+});
+
+app.get('/secret', function(request, response) {
+	response.send('This is a secret.');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
