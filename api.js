d8229b399ecafaffae5e8363c95d00fe7d322c0f
foslock <foslock@gmail.com>
2013-04-07 21:20:32 -0400

    Added a secret

--- api.js
+++ api.js
@@ -1,6 +1,11 @@
 var express = require('express');
+var mongo = require('mongodb');
 
-var app = express.createServer(express.logger());
+var mongoUri = process.env.MONGOHQ_URL || 
+  'mongodb://heroku:84c7ca002fa6079a82bad84714a0beb7@linus.mongohq.com:10093/app14677217'; 
+
+var app = express.createServer();
+app.use(express.logger('dev'));
 
 app.get('/', function(request, response) {
 	response.send('You\'re totally on a plane. It\'s in the sky. It\'s going fast.');
@@ -14,3 +19,5 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
 	console.log("Listening on " + port);
 });
+
+
