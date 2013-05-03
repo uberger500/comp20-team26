a431fdacf4ffe55bebd38c6dea5c04407f1340f0
sampurcell93 <spurcell93@gmail.com>
2013-05-03 01:05:59 -0400

    last night

--- www/globals.js
+++ www/globals.js
@@ -72,17 +72,29 @@ $(document).ready(function() {
 
 	User.prototype.loginSuccess = function() {
 		if (!callct){
-			reload = setInterval(getLastTenLines, 500);
+			reload = window.setInterval(getLastTenLines, 500);
 			$("#trigger-input").trigger("click");
 		}
-		$(".before-login").animate({height: "hide"}, 400, function() {
-			google.maps.event.trigger(map, 'resize');
-		});
+		$(".before-login").animate({width: "hide", height: "hide"}, 200);
 		$(".after-login").fadeIn("slow");
 		$(".drop-down").fadeIn("slow");
 		reload = setInterval(getLastTenLines, 500);
+		google.maps.event.trigger(map, 'resize');
 		return this;
 	};
+
+	User.prototype.updateFlight = function(flightnum) {
+		$.get("http://127.0.0.1:5000/api/currentdata", { 
+			flight: logged_user.flightnum || flightnum,
+			token: logged_user.token
+		}, function(data) {
+			console.log(data);
+			if (typeof data.altitude !== "undefined") {
+				logged_user.curr_flight = data;
+				logged_user.flightnum = flightnum;
+			}
+		});
+	}
 
 	// Load saved user if there is one
 	if (localStorage.savedUser) {
@@ -100,13 +112,6 @@ $(document).ready(function() {
 		$("#snake-board").focus();
 	});
 
-	function Graph() {
-		return this;
-	}
-
-	Graph.prototype.addNode = function() {
-		return this;
-	};
 
 	function drawMyChart() {
         if(!!document.createElement('canvas').getContext){ //check that the canvas
@@ -163,17 +168,15 @@ $(document).ready(function() {
 
 	$("#submit-flight").on("click", function(e) {
 		var flightnum = $("#flight-number").val();
+		console.log(flightnum);
 		if (flightnum === "") {
 			e.preventDefault();
 			e.stopPropagation();
 			return;
 		}
-		$.get("http://127.0.0.1:5000/api/currentdata", {
-			flight: flightnum,
-			token: logged_user.token
-		}, function(data) {
-			console.log(data);
-		});
+		logged_user.updateFlight(flightnum);
+		flightupdate = window.setInterval(logged_user.updateFlight, 200000);
+
 	});
 
 	function submitChat()
