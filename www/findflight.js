0c91533bdc7eb1a53b05b0541868f5ae8d0908cd
cvrett01 <charles.vrettos@tufts.edu>
2013-05-14 01:25:24 -0400

    local -> wingman for deployment

--- www/findflight.js
+++ www/findflight.js
@@ -19,7 +19,7 @@ function displaynearbyplanes(){
 			// to a get request that respons with json planes
 			
 			//change this to wingmanapi.herokuapp whatever
-			var url = "http://127.0.0.1:5000/api/nearbyplanes?latitude=" + myLoc.lat() + "&longitude=" + myLoc.lng() + "&token=" + logged_user.token;
+			var url = "http://wingmanapi.herokuapp.com/api/nearbyplanes?latitude=" + myLoc.lat() + "&longitude=" + myLoc.lng() + "&token=" + logged_user.token;
 			// console.log(url);
 			$.get(url, function(data){ 
 				// data comes back as an array of flight strings
@@ -52,7 +52,7 @@ function displaynearbyplanes(){
 						var numverified = 0;
 						
 						var flightpluses = flight.replace(/ /g, "+");
-						var url = "http://127.0.0.1:5000/api/checkflight?flight=" + flightpluses + "&token=" + logged_user.token;
+						var url = "http://wingmanapi.herokuapp.com/api/checkflight?flight=" + flightpluses + "&token=" + logged_user.token;
 						//check if all flights are valid on wolfram and are either en route or havent taken off yet; landeds will not be returned
 						$.ajax({
 							url: url,
@@ -105,7 +105,7 @@ function submitflight(){
 	
 	//check if valid on wolfram
 	var flightpluses = flightnum.replace(/ /g, "+");
-	var url = "http://127.0.0.1:5000/api/checkflight?flight=" + flightpluses + "&token=" + logged_user.token;
+	var url = "http://wingmanapi.herokuapp.com/api/checkflight?flight=" + flightpluses + "&token=" + logged_user.token;
 	//check if all flights are valid on wolfram and are either en route or havent taken off yet; landeds will not be returned
 	$.get(url, function(response){ 
 		if (response.status == "valid" || response.status == "has not taken off yet"){
@@ -114,7 +114,7 @@ function submitflight(){
 			}
 			
 			//add flight to database
-			$.post("http://127.0.0.1:5000/api/user/addflight", {username: logged_user.email, token: logged_user.token, flight: flightnum});
+			$.post("http://wingmanapi.herokuapp.com/api/user/addflight", {username: logged_user.email, token: logged_user.token, flight: flightnum});
 
 	
 			logged_user.Set("flightnum",flightnum);
